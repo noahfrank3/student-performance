a4c523b02e399a82f116707f8d7189753f4b3b07
noahfrank3 <noahfrank3@outlookc.om>
2025-04-27 20:05:48 -0400

    Finish script

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 from pathlib import Path
 
 from git import Repo
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import pearsonr
 
-def load_data()
-    repo = Repo('.', search_parent_directories=True)
-    repo = Path(repo.git.rev_parse("--show-toplevel"))
+REPO = Path(Repo('.', search_parent_directories=True).git.rev_parse("--show-toplevel"))
 
-    data_dir = repo / 'data'
-    data_file = data_dir / 'data.npz'
+DATA_DIR = REPO / 'data'
+OUT_DIR = REPO / 'output'
 
-    try:
-        data = np.load(data_file)
-    except FileNotFoundError:
-        def get_xlsx_data(filename):
-            path = data_dir / filename
-            df = pd.read_excel(path)
+def correlation_plot(data, subject_name, grade_num):
+    fig, ax = plt.subplots()
+    ax.scatter(data['absences'], data[f'G{grade_num}'], s=20, color='dodgerblue', zorder=3)
+    ax.set_xlabel("Number of Absences", fontsize='large')
+    ax.set_ylabel(f"Grade (G{grade_num})", fontsize='large')
+    ax.set_title(f"Grade vs. Number of Absences, {subject_name.title()}", fontsize='large')
+    ax.grid(True, zorder=0)
+    fig.tight_layout()
+    fig.savefig(OUT_DIR / f'{subject_name[:4]}_g{grade_num}.svg', bbox_inches='tight')
+    plt.close(fig)
 
-            absences = df['absences'].to_numpy()
-            grades = df['G3'].to_numpy()
-
-            return absences, grades
-
-        math_absences, math_grades = get_xlsx_data('student-mat.xlsx')
-        port_absences, port_grades = get_xlsx_data('student-por.xlsx')
-
-        np.savez(
-                data_file,
-                math_absences=math_absences,
-                math_grades=math_grades,
-                port_absences=port_absences,
-                port_grades=port_grades
-        )
-        data = np.load(data_file)
-
-    return data
+rho_math = np.empty(3)
+p_math = np.empty(3)
+rho_port = np.empty(3)
+p_port = np.empty(3)
 
 if __name__ == '__main__':
-    data = load_data()
+    math_data = pd.read_excel(DATA_DIR / 'student-mat.xlsx')
+    port_data = pd.read_excel(DATA_DIR / 'student-por.xlsx')
+    
+    for grade_num in range(1, 4):
+        correlation_plot(math_data, 'math', grade_num)
+        rho_math[grade_num - 1], p_math[grade_num - 1] = pearsonr(math_data['absences'], math_data[f'G{grade_num}'])
+
+        correlation_plot(port_data, 'portuguese', grade_num)
+        rho_port[grade_num - 1], p_port[grade_num - 1] = pearsonr(port_data['absences'], port_data[f'G{grade_num}'])
+
+    corr_data = {
+            'math': {
+                'rho': rho_math,
+                'p': p_math
+            },
+            'port': {
+                'rho': rho_port,
+                'p': p_port
+            }
+    }
+
+    print(corr_data)
